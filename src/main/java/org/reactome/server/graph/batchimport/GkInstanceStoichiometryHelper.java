892324872983c6bd8ceff7de6658e9c8f2e19230
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-08-02 16:10:15 +0100

    cleanup

--- src/main/java/org/reactome/server/graph/batchimport/GkInstanceStoichiometryHelper.java
+++ src/main/java/org/reactome/server/graph/batchimport/GkInstanceStoichiometryHelper.java
@@ -3,30 +3,27 @@ package org.reactome.server.graph.batchimport;
 import org.gk.model.GKInstance;
 
 /**
- * Created by:
- *
  * @author Florian Korninger (florian.korninger@ebi.ac.uk)
- * @since 18.01.16.
  */
 class GkInstanceStoichiometryHelper {
 
     private final GKInstance instance;
     private Integer count;
 
-    public GkInstanceStoichiometryHelper(GKInstance instance) {
+    GkInstanceStoichiometryHelper(GKInstance instance) {
         this.instance = instance;
         this.count = 1;
     }
 
-    public GKInstance getInstance() {
+    GKInstance getInstance() {
         return instance;
     }
 
-    public Integer getCount() {
+    Integer getCount() {
         return count;
     }
 
-    public void increment() {
+    void increment() {
         count++;
     }
 }
